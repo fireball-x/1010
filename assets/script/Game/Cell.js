c1c54fd417f7f60b2870f1a06a48431c8f09dfc2
knoxHuang <821686174@qq.com>
2015-02-10 14:18:48 +0800

    Update Board and Cell

--- assets/script/Game/Cell.js
+++ assets/script/Game/Cell.js
@@ -1,6 +1,6 @@
 var Cell = Fire.defineComponent();
 
-Cell.prop('pos', new Vec2(0, 0));
+Cell.prop('pos', new Fire.Vec2(0, 0));
 Cell.prop('hasCube', false);
 
 Cell.prototype.clean = function() {
@@ -8,12 +8,12 @@ Cell.prototype.clean = function() {
 };
 
 Cell.prototype.setPos = function(x, y) {
-    this.pos = new Vec2(x, y);
+    this.pos = new Fire.Vec2(x, y);
 };
 
 Cell.prototype.setCube = function(cube){
     cube.parent = this.entity;
-    cube.transform.position = new Vec2(0, 0);
+    cube.transform.position = new Fire.Vec2(0, 0);
 	this.hasCube = true;
 };
 
