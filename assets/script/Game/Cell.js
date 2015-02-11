ad7cea0b11a6d4996ff09e77462d3a0f62cfe36a
knoxHuang <821686174@qq.com>
2015-02-11 11:24:25 +0800

    更新 Board.js Cell.js Game.js

--- assets/script/Game/Cell.js
+++ assets/script/Game/Cell.js
@@ -2,23 +2,25 @@ var Cell = Fire.defineComponent();
 
 Cell.prop('pos', new Fire.Vec2(0, 0));
 Cell.prop('hasCube', false);
+Cell.prop('_cube', null, Fire.HideInInspector);
 
 Cell.prototype.clean = function() {
     this.entity.destroy();
 };
 
-Cell.prototype.setPos = function(x, y) {
-    this.pos = new Fire.Vec2(x, y);
-};
-
-Cell.prototype.setCube = function(cube){
-    cube.parent = this.entity;
+Cell.prototype.putCube = function (cube) {
+    cube.entity.parent = this.entity;
     cube.transform.position = new Fire.Vec2(0, 0);
-	this.hasCube = true;
+    this.hasCube = true;
+    this._cube = cube;
 };
 
 Cell.prototype.onLoad = function() {
     //-- 绑定Cube销毁消息
+    this.entity.on("cube destroyed", function () {
+        this._cube = null;
+        this.hasCube = false;
+    });
 };
 
 module.exports = Cell;
--- assets/script/Game/Game.js
+++ assets/script/Game/Game.js
@@ -1,2 +1,106 @@
-var Game = Fire.defineComponent();
+var Board = require('Board');
+var Cell = require('Cell');
+var Cube = require('Cube');
+var CubeGroup = require('CubeGroup');
+
+var Game = Fire.defineComponent(function () {
+    this.board = null;
+    this.tempCube = null;
+});
+
+
+Game.prototype.onLoad = function () {
+	//-- 创建格子到棋盘上
+    if (!this.tempCube) {
+        this.tempCube = this.entity.find('../Prefabs/Cube');
+    }
+    var boardObj = this.entity.find('../Board');
+    this.board = boardObj.getComponent(Board);
+    this.board.create();
+    
+    boardObj.on('mousedown', function (event) {
+        var cube = null;
+        var cell = event.target.getComponent(Cell);
+        if (!cell) {
+            cube = event.target.getComponent(Cube);
+            if (cube) {
+                cube.Clear();
+            }
+        }
+        else {
+            var groupObj = this.entity.find('../CubeGroup');
+            var group = groupObj.getComponent(CubeGroup);
+            var cubGroup = group.create(32, CubeGroup.prototype.gridType.Line_5_90);
+            var center = cell;
+            var child = [];
+            for (var i = 0; i < cubGroup._children.length; ++i) {
+                child.push(cubGroup._children[i]);
+            }
+            var hasPutCube = this.board.canPutCubeToCell(cubGroup, center);
+            if (hasPutCube) {
+                for (var j = 0, len = cubGroup._children.length; j < len; j++) {
+                    cube = child[j].getComponent(Cube);
+                    var pos = cube.position;
+                    cell = this.board.getCell(center.pos.x + pos.x, center.pos.y + pos.y);
+                    cell.putCube(cube);
+                }
+
+            }
+        }
+    }.bind(this));
+};
+
+Game.prototype.delline = function(cubeX , cubeY){
+
+    var clearRow = true, clearCol = true;
+    var cell = null;
+    var x = 0, y = 0, len = cubeX;
+    var delCubeList = [];
+    for (x = 0; x < len; ++x) {
+        cell = this.board.getCell(x, cubeY);
+        if (!cell.hasCube) {
+            clearRow = false;
+            break;
+        }
+        else {
+            delCubeList.push(cell.child)
+        }
+    }
+    if (clearRow) {
+        len = Math.abs(cubeX - this.board.count.x);
+        for (x = 0; x < len; ++x) {
+            cell = this.board.getCell(x, cubeY);
+            if (!cell.hasCube) {
+                clearRow = false;
+                break;
+            }
+        }
+    }
+    for (y = 0, len = cubeY; y < len; ++y) {
+        cell = this.board.getCell(cubeX, y);
+        if (!cell.hasCube) {
+            clearCol = false;
+            break;
+        }
+    }
+    if (clearCol) {
+        len = Math.abs(cubeY - this.board.count.y);
+        for (y = 0; y < len; ++y) {
+            cell = this.board.getCell(cubeX, y);
+            if (!cell.hasCube) {
+                clearCol = false;
+                break;
+            }
+        }
+    }
+};
+
+Game.prototype.onStart = function () {
+
+};
+
+Game.prototype.update = function () {
+
+};
+
 module.exports = Game;
