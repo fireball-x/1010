0ec122f0777fbc7a0b30feb2f2d837cfc5acc420
Jare Guo <jare85@163.com>
2015-02-24 21:15:11 +0800

    refine

--- assets/script/Game/Background.js
+++ assets/script/Game/Background.js
@@ -3,7 +3,7 @@ var Background = Fire.defineComponent();
 Background.prop('backGroups', [], Fire.ObjectType(Fire.Sprite));
 
 Background.prototype.onLoad = function() {
-    var bgSprite = this.backGroups[Math.floor(Math.random() * this.backGroups.length)];
+    var bgSprite = this.backGroups[Math.randomRangeInt(0, this.backGroups.length)];
     
     var render = this.entity.getComponent(Fire.SpriteRenderer);
     render.sprite = bgSprite;
